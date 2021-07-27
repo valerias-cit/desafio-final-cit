8b0b548ef33275253157037cba2d4040ebed7616
valerias-cit <valerias@ciandt.com>
2021-07-27 10:45:08 -0300

    setup projeto

---
+++ candidateCIandT/src/main/java/br/com/candidate/candidateCIandT/CandidateCIandTApplication.java
@@ -0,0 +1,13 @@
+package br.com.candidate.candidateCIandT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateCIandTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateCIandTApplication.class, args);
+	}
+
+}
---
+++ candidateCIandT/src/test/java/br/com/candidate/candidateCIandT/CandidateCIandTApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.candidate.candidateCIandT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateCIandTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
