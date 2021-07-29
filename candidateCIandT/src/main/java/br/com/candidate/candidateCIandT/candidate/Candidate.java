b101e97e73bfc58df7bbe36b91c3bbb0a433e153
Rafaela de Jesus Alves Candido <CIANDT\rcandido@lnb027960cps.ciandt.global>
2021-07-29 15:22:46 -0300

    [BLUO-572][BLUO-615] Correção da mensagem de erro na criação de candidatos

--- candidateCIandT/src/main/java/br/com/candidate/candidateCIandT/candidate/Candidate.java
+++ candidateCIandT/src/main/java/br/com/candidate/candidateCIandT/candidate/Candidate.java
@@ -5,8 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 import static javax.persistence.GenerationType.IDENTITY;
