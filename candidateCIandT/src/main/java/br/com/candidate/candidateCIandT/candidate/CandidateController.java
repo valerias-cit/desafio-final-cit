9e66a813ba1772e9254a806cac757106a3065737
Rafaela de Jesus Alves Candido <CIANDT\rcandido@lnb027960cps.ciandt.global>
2021-07-29 11:24:19 -0300

    [BLUO-571][BLUO-588][BLUO-591] Criação do findCandidates no Controleer e seus testes

--- candidateCIandT/src/main/java/br/com/candidate/candidateCIandT/candidate/CandidateController.java
+++ candidateCIandT/src/main/java/br/com/candidate/candidateCIandT/candidate/CandidateController.java
@@ -4,13 +4,11 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/candidate")
@@ -45,5 +43,10 @@ public class CandidateController {
         } catch (Exception exception) {
             throw exception;
         }
+    }
+
+    @GetMapping
+    public List<Candidate> findCandidates() {
+        return candidateService.findByStatus(Status.A);
     }
 }
--- candidateCIandT/src/test/java/br/com/candidate/candidateCIandT/CandidateCIandTApplicationTests.java
+++ candidateCIandT/src/test/java/br/com/candidate/candidateCIandT/CandidateCIandTApplicationTests.java
@@ -1,23 +1,30 @@
 package br.com.candidate.candidateCIandT;
 
-import br.com.candidate.candidateCIandT.candidate.CandidateController;
-import br.com.candidate.candidateCIandT.candidate.CandidateRequest;
-import br.com.candidate.candidateCIandT.candidate.Level;
-import br.com.candidate.candidateCIandT.candidate.Status;
+import br.com.candidate.candidateCIandT.candidate.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static br.com.candidate.candidateCIandT.candidate.Level.Junior;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,14 +33,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class CandidateCIandTApplicationTests {
 
-	@InjectMocks
-	private CandidateController candidateController;
-
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
 	private ObjectMapper objectMapper;
+
+	@MockBean
+	private CandidateService candidateService;
 
 	CandidateRequest candidateRequest = new CandidateRequest("Rafaela Candido", "software engineer", Level.Junior, 80, "rafaelacandido.linkedIn", Status.A);
 
@@ -65,7 +72,61 @@ class CandidateCIandTApplicationTests {
 				.andExpect(jsonPath("$[0].field").value("fullName"));
 	}
 
+	@Test
+	void listarCandidatosAtivos() throws Exception {
+		//Dado que exista uma lista de candidatos
+		List<Candidate> listaEsperada = Lists.list(Candidate.builder()
+						.id(100L)
+						.fullName("Mario")
+						.description("software engineer")
+						.level(Junior)
+						.proficiency(80)
+						.socialLinks("mario.linkedIn.com")
+						.createdAt(LocalDateTime.now())
+						.status(Status.A)
+						.build(),
+				Candidate.builder()
+						.id(200L)
+						.fullName("João")
+						.description("architect")
+						.level(Junior)
+						.proficiency(90)
+						.socialLinks("joao.linkedIn.com")
+						.createdAt(LocalDateTime.now())
+						.status(Status.A)
+						.build());
+
+		//E o service.findByStatus retorne esta lista
+		Mockito.when(candidateService.findByStatus(Mockito.any())).thenReturn(listaEsperada);
+
+		//Quando o controller.findCandidates for chamado
+		mockMvc.perform(MockMvcRequestBuilders.get("/candidate")
+						.header("Accept-Language", "pt-BR")
+						.characterEncoding("UTF-8"))
+						.andExpect(status().isOk()) // então a resposta deve ser 200ok
+						.andExpect(jsonPath("$").value(hasSize(2))) // e a lista deve ter 2 items
+						.andExpect(jsonPath("$[0].fullName").value("Mario")) //e o primeiro item deve ter o nome Mario
+						.andExpect(jsonPath("$[1].fullName").value("João")); //e o segundo item deve ter o nome João
+	}
+
+	@Test
+	void listaVazia() throws Exception {
+		//Dado que exista uma lista vazia de candidatos
+		List<Candidate> listaEsperada = Lists.emptyList();
+
+		//E o service.findByStatus retorne esta lista
+		Mockito.when(candidateService.findByStatus(Mockito.any())).thenReturn(listaEsperada);
+
+		//Quando o controller.findCandidates for chamado
+		mockMvc.perform(MockMvcRequestBuilders.get("/candidate")
+						.header("Accept-Language", "pt-BR")
+						.characterEncoding("UTF-8"))
+						.andExpect(status().isOk()) // então a resposta deve ser 200ok
+						.andExpect(jsonPath("$").value(hasSize(0))); //e a lista deve estar vazia
+	}
+
 	public String geradorJson(CandidateRequest candidateRequest) throws JsonProcessingException {
 		return objectMapper.writeValueAsString(candidateRequest);
 	}
+
 }
